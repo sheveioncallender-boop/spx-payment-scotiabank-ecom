c45997d0c6b12f6d372437bdd4d3bed5a39b6f79
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-09-16 18:08:35 -0400

    Show PayBridge in Odoo's default Apps list

--- spx_paybridge_scotia/__manifest__.py
+++ spx_paybridge_scotia/__manifest__.py
@@ -28,5 +28,5 @@ advertised because this integration uses the hosted sale protocol only.
     'post_init_hook': 'post_init_hook',
     'uninstall_hook': 'uninstall_hook',
     'installable': True,
-    'application': False,
+    'application': True,
 }
